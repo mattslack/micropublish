73500144a27603891cb703cd78f3d83f32add5a6
Matt Slack <matt@mattslack.org>
2023-10-10 23:43:25 -0400

    Ditch the tokenfield and autogrow js for now

--- lib/micropublish/helpers.rb
+++ lib/micropublish/helpers.rb
@@ -1,24 +1,23 @@
 module Micropublish
   module Helpers
-
     def h(text)
       Rack::Utils.escape_html(text)
     end
 
     def flash_message
-      if session.key?('flash') && !session[:flash].empty?
-        content = %Q{
+      if session.key?("flash") && !session[:flash].empty?
+        content = %(
           <div class="alert alert-#{session[:flash][:type]}">
             #{session[:flash][:message]}
           </div>
-        }
-        session.delete('flash')
+        )
+        session.delete("flash")
         content
       end
     end
 
     def default_format
-      if session.key?('format') && session[:format] == :form
+      if session.key?("format") && session[:format] == :form
         :form
       else
         :json
@@ -26,29 +25,19 @@ module Micropublish
     end
 
     def autogrow_script(id)
-      %Q{
-        <script>
-          $(function(){
-            $('##{id}').autogrow({vertical: true, horizontal: false});
-          });
-        </script>
-      }
     end
 
     def tokenfield_script(id)
-      %Q{
-        <script>
-          $(function(){
-            $('##{id}').tokenfield();
-          });
-        </script>
-      }
+      # The old tokenfield package was abandoned in 2015 or so, and depended
+      # on jQuery.
+      # If we bring back tokenfield functionality we should use something newer
+      # https://github.com/KaneCohen/tokenfield is promising, but a slightly
+      # heavier payload than I'd like for something that's not core functionality
     end
 
     def tweet_reply_prefix(tweet_url)
       tweet_match = tweet_url.to_s.match(/twitter\.com\/([A-Za-z0-9_]+)\//)
       tweet_match.nil? ? "" : "@#{tweet_match[1]} "
     end
-
   end
 end
